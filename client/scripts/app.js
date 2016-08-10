20311feb83ed5352e51eb073258d839b2e65f8b9
Truong-Julie <gamerlazer@gmail.com>
2016-08-09 19:54:46 -0700

    Complete Chatter Box - roomSelect

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,38 +1,21 @@
 // YOUR CODE HERE:
-
-var Message = function(messageText, userName, roomName) {
-  this.text = messageText;
-  this.username = userName;
-  this.roomname = roomName;
-};
-
-$.ajaxSetup({
-  dataFilter: function(data) {
-    var data = JSON.parse(data);
-    // put filtering here
-    return JSON.stringify(data);
-  }
-});
-
 var app = {
-  server: 'https://api.parse.com/1/classes/messages'
+  server: 'https://api.parse.com/1/classes/messages',
+  messages: [],
+  allRooms: {},
+  roomName: 'lobby',
 };
 
 app.init = function() {
-// what happens when the window loads
-  // $('.username').on('click', function (data) {
-  //   console.log('Username clicked!');
-  //   app.addFriend(this);
-  // });
+  
+  // listeners
+  $('#roomSelect').on('change', app.handleRoomChange);
 
   $('body').on('click', '.username', function (data) {
-    console.log('Username clicked!');
     app.addFriend(this);
   });
 
   $('#send .submit').on('click', function (event) {  // #send .submit
-    // console.log('Submit clicked: ', event);
-    // console.log(document.getElementById('messageBox').value);
 
     // Grabs user input 
     var userMessage = $(':input:odd').val();
@@ -44,8 +27,41 @@ app.init = function() {
     // console.log(userMessage);
   });
 
+  // begin auto-fetching
+  app.fetch();
+  setInterval(app.fetch, 6000);
+
 };
 
+
+
+
+// **************** AJAX ******************* // 
+
+app.fetch = function() {
+  $.ajax({
+    url: app.server,
+    // set ajax response to text
+    contentType: 'application/json',  // dataType: 'text',
+    data: { order: '-createdAt'},
+    type: 'GET',
+    async: true,
+    statusCode: {
+      404: function (response) {
+        alert(404);
+      },
+      200: function (response) {
+        app.messages = response['results'];
+
+        // process to render messages
+        app.renderMessages(app.messages);
+      }
+    },
+    error: function (jqXHR, status, errorThrown) {
+      alert('error', errorThrown);
+    }
+  });
+};
 
 app.send = function(message) {
   $.ajax({
@@ -64,95 +80,60 @@ app.send = function(message) {
   });
 };
 
-// app.send(message);
-var rawData;
 
-/*********** OG Fetch Function **************/
-app.fetch = function() {
-  // $.ajax(app.server);
-  $.get(app.server, function(data) { 
-    rawData = data['results'];
-    _.each(rawData, function(item) {
-      app.addMessage(item);
-      console.log('item object', item);
-    });
-    console.log('inner function', rawData[0]);
-    // data['results'] <-- array of all messages
-    // return data;
-  });
+
+// *************** Messages ***************//
+
+var Message = function(messageText, userName, roomName) {
+  this.text = messageText;
+  this.username = userName;
+  this.roomname = roomName;
 };
-/****************** Test Fetch Function ************************/
-// Goal to return text instead of a JSON file
-
-app.fetch = function() {
-  $.ajax({
-    url: app.server,
-    dataType: 'text',
-    type: 'GET',
-    async: true,
-    statusCode: {
-      404: function (response) {
-        alert(404);
-      },
-      200: function (response) {
-        // filtering
-          //for the block of text if we encounter < replace with 
-          // replace for <, &, >, and 
-          // response.sp
-        // console.log(response);
-
-        var escapedResponse = response.replace(/</gi, '&lt;');
-        escapedResponse = escapedResponse.replace(/>/gi, '&gt;');
-        escapedResponse = escapedResponse.replace(/&/gi, '&amp;');
-        // console.log(response.replace(/results/gi, "resmults"));
-        // parse escaped repose
-        parsedResponse = JSON.parse(escapedResponse);   
-        // console.log(parsedResponse); 
-
-        _.each(parsedResponse['results'], function (item) {
-          app.addMessage(item);
-          console.log('item object', item);
-        });
-        // for each item 
-          // call app add message on message
-      }
-    },
-    error: function (jqXHR, status, errorThrown) {
-      alert('error');
-    }
-  });
-};
-
 
 app.clearMessages = function() {
   $('#chats').children().remove();
 };
 
+app.renderMessages = function(messages) {
+  app.clearMessages();
+  messages
+    .filter(function(message) { 
+      if (app.roomName === 'lobby' && !message.roomname) { 
+        return true; 
+      } else if (message.roomname === app.roomName) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .forEach(function(item) {
+      app.addMessage(item);
+    });
 
-var message = {
-  username: 'temp',
-  text: 'test',
-  roomname: 'test'
+  // iterate through each new message and...
+  // messages.forEach(function(item) {
+  //   app.addMessage(item);
+  // });
+  
+  app.renderRoomList(messages);
 };
-/*
-{createdAt: "2016-08-09T03:04:03.556Z", objectId: "KAHVFZjZ4U", roomname: "Room 9", text: "hello", updatedAt: "2016-08-09T03:04:03.556Z"…}
-*/
 
 app.addMessage = function(message) {
-  $('#chats').append('<div class="message">' + '<span class="username">' + message.username + '</span>' + ': ' + message.text + '</div>'); // '</div><div class="message">'
-  
-  // $('.username').on('click', function() {
-  //   console.log('Initiated username');
-  // });
-};
 
-// $('.username').on
+  var $chat = $('<div class="chat" />');
 
-app.addRoom = function(roomName) {
-  $('#roomSelect').append('<option value=' + roomName + '>' + roomName + '</option>');   
-};
+  var $username = $('<span class="username" />');
+  $username
+    .text(message.username + ': ')
+    .attr('data-username', message.username)
+    .appendTo($chat);
 
-app.addFriend = function() {
+  var $message = $('<br><span/>');
+  $message
+    .text(message.text)
+    .appendTo($chat);
+
+  $('#chats').append($chat);
 
 };
 
@@ -160,16 +141,42 @@ app.handleSubmit = function(messageText, userName, roomName) {
   var newMessage = new Message(messageText, userName, roomName);
   console.log(newMessage);
   app.send(newMessage);
+  // add message immediately 
+  app.addMessage(newMessage);
   // make addMessage() <-- add to the DOM
 };
 
 
+// ******************** Rooms **********************// 
 
+app.addRoom = function(roomName) {
+  $('#roomSelect').append('<option value=' + roomName + '>' + roomName + '</option>');   
+};
 
+app.renderRoomList = function(messages) {
+  console.log('renderRoomList run');
+  // for each message
+  messages.forEach(function(message) {
+    // add a key to a obj, to remove duplicate rooms
+    app.allRooms[message.roomname] = true;
+  });
+  // for unique room call addRoom on room
+  $('#roomSelect').html('');
+  $('#roomSelect').append('<option selected value="">Select a room</option>');
+  $('#roomSelect').append('<option value="Add Room">Add New Room</option>');
+  for (var room in app.allRooms) {
+    app.addRoom(room);
+  }
+};
 
-$(document).ready(function() {
-  app.init();
-});
+app.handleRoomChange = function() {
+  $('#roomSelect').prop('selectedIndex');
+};
 
+// ******************* Friends ******************// 
 
-// $(document).   ready(app.init.bind(app));
+app.addFriend = function() {
+  // click on a name
+
+};
+
